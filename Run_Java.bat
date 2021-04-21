cca3b59494d832321155b2b9b6d7efa458ece7f9
Welri <52676712+Welri@users.noreply.github.com>
2021-04-21 14:02:30 +0200

    Subprocess Editing
    There was some issues when switching between Windows and Ubuntu that the DARP.java and ConnectComponent.java in the darp_pack had to be recompiled.
    Those compilations are now included in the subprocess file. It might be useful to have a separate subprocess file that runs once, but not sure how to implement that.

--- Run_Java.bat
+++ Run_Java.bat
@@ -3,6 +3,7 @@
 @REM cd W:\Documents\DOCUMENTS\Visual\DARP_Testing
 @REM java -cp DARP_JPype DARP_Java_Main
 @REM cd DARP_Java
+javac darp_pack/ConnectComponent.java
 javac darp_pack/DARP.java
 javac DARP_Java_New.java
 java DARP_Java_New
